568ff09d9e31cfde8991d4b333668e3dc67b207f
AyushiAnderiya <ayushianderiya14@gmail.com>
2024-03-06 18:05:25 +0530

    creating react file and vite file

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Starting React | Ayushi Anderiya</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>React with Vite | Ayushi Anderiya</h1>
+  )
+}
+
+export default App
