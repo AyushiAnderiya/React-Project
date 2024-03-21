b24750a87fa44e3c69f0eb40192ce3ecaf938d2e
AyushiAnderiya <ayushianderiya14@gmail.com>
2024-03-21 22:31:39 +0530

    importing Images

--- netflix/src/main.jsx
+++ netflix/src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import data from './NData.jsx';
+import logo from './'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <>
-    <div>
-    <p id="headd" className="text-4xl text-red-500 tracking-wider space-x-2">
+    <div id="headd">
+    <p  className="text-4xl text-red-500 tracking-wider space-x-2">
         Netflix & Chill 
       </p>
     </div>
