7ef67c073abd505e15d5e61fb0bdc53cdfcdd505
AyushiAnderiya <ayushianderiya14@gmail.com>
2024-03-30 02:10:16 +0530

    Update Date

--- Clock/src/App.jsx
+++ Clock/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { format } from 'date-fns';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     
-    </>
-  )
+function App(){
+  let [tdate,setdate]=useState(0);
+  const handleClick=()=>{
+    tdate=new Date();
+    let formatted=format(tdate,"dd/MM/yyyy")
+    setdate(formatted);
+    console.log(formatted);
+  }
+return(
+  <>
+  <h1>{tdate}</h1>
+  <button onClick={handleClick}>Click</button>
+  </>
+)
 }
 
 export default App
